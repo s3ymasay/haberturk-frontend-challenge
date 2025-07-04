6d94dbd3b255cfdd00554a0e719b69ee66858875
s3ymasay <s3ymasay@gmail.com>
2025-07-04 19:17:40 +0300

    Add GitHub Pages configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  // GitHub Pages repository adınızı buraya yazın
+  basePath: process.env.NODE_ENV === 'production' ? '/haberturk-frontend-challenge' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/haberturk-frontend-challenge' : '',
+};
 
 export default nextConfig;
